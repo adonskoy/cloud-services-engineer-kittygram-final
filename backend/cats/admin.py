bb30dcf18f10bc12ea0692feeac30c55b84ec179
Anton Donskoy <a.donskoy@inostudio.com>
2026-03-09 23:54:32 +0300

    register cats models in django admin

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Achievement, AchievementCat, Cat
+
+
+class AchievementCatInline(admin.TabularInline):
+    model = AchievementCat
+    extra = 0
+
+
+@admin.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'birth_year', 'owner')
+    list_filter = ('color', 'birth_year')
+    search_fields = ('name', 'owner__username')
+    inlines = [AchievementCatInline]
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(AchievementCat)
+class AchievementCatAdmin(admin.ModelAdmin):
+    list_display = ('cat', 'achievement')
+    list_filter = ('achievement',)
